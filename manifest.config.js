daf100703a26516a5b7b6ad40bfbf4e2eb9fa429
Fourchaite <binglerlee@gmail.com>
2025-12-13 09:05:08 +0100

    style: remove comment + fix indent

--- manifest.config.js
+++ manifest.config.js
@@ -6,9 +6,9 @@ export default defineManifest({
   name: pkg.name,
   version: pkg.version,
   icons: {
-    24: 'icons/icon24.ico', // FIXED: Removed 'public/'
-    48: 'icons/icon48.ico', // FIXED: Removed 'public/'
-    96: 'icons/icon96.ico', // FIXED: Removed 'public/'
+    24: 'icons/icon24.ico',
+    48: 'icons/icon48.ico',
+    96: 'icons/icon96.ico',
   },
   permissions: [
     "tabs",
@@ -19,9 +19,9 @@ export default defineManifest({
   ],
   action: {
     default_icon: {
-     24: 'icons/icon24.ico', // FIXED: Removed 'public/'
-      48: 'icons/icon48.ico', // FIXED: Removed 'public/'
-      96: 'icons/icon96.ico', // FIXED: Removed 'public/'
+      24: 'icons/icon24.ico',
+      48: 'icons/icon48.ico',
+      96: 'icons/icon96.ico',
     },
   },
   content_scripts: [{
