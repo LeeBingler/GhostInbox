5db7ea0c46ee8ec85b3dfb38d0099d7f8165ab8b
Fourchaite <binglerlee@gmail.com>
2025-12-24 12:50:39 +0100

    feat: add html injection of the extension sidepanel only when the user click the extension icon

--- src/content/main.jsx
+++ src/content/main.jsx
@@ -2,13 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Sidebar from './sidebar/Sidebar.jsx'
 
-const container = document.createElement('div')
-container.id = 'crxjs-app'
-document.body.appendChild(container)
-createRoot(container).render(
-  <StrictMode>
-    <Sidebar/ >
-  </StrictMode>,
-)
 
-chrome.runtime.sendMessage({ type: "READY_FOR_EMAILS" });
+chrome.runtime.onMessage.addListener((msg) => {
+    const idContainer = "ghostinbox-app"
+    let container = document.getElementById(idContainer);
+
+    if (msg.type === "OPEN_SIDEBAR" && !container) {
+        container = document.createElement('div');
+        container.id = idContainer;
+        document.body.appendChild(container);
+        createRoot(container).render(
+          <StrictMode>
+              <Sidebar />
+          </StrictMode>,
+        );
+
+        chrome.runtime.sendMessage({ type: "READY_FOR_EMAILS" });
+    }
+})
